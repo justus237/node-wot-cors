7e610a8b7437cc1355392ada1d9cd02a63b07b26
justus237 <62303964+justus237@users.noreply.github.com>
2023-03-06 18:32:21 +0100

    update servers

--- server-longpoll.js
+++ server-longpoll.js
@@ -1,39 +1,31 @@
 const { Servient, Helpers } = require("@node-wot/core");
 const { HttpServer } = require("@node-wot/binding-http");
 const httpConfig = {
-	serverKey: "server.key",
-	serverCert: "server.cert",
-	security: {
-		scheme: "basic", // (username & password)
-	},
 	port: 8082,
 }
 
+
 let thingDescriptionObject = {
-	id: "urn:uuid:8b82d730-44af-4305-ba01-c487ff5af1a0",
-	title: "some thing",
+	id: "urn:uuid:8b82d730-44af-4305-ba01-c487ff5af1a1",
+	title: "some other thing",
 	description: "a description of the thing",
 	securityDefinitions: {
-		"basic_sc": {
-			scheme: "basic", //nosec
+		"nosec_sc": {
+			scheme: "nosec", //nosec
 		},
 	},
-	security: "basic_sc",
+	security: "nosec_sc",
 	properties: {
 		"status": {
 			"type": "string",
-			"readOnly": true
+			"readOnly": true,
+			"observable": true
 		}
 	}
 }
 
 const servient = new Servient();
-servient.addCredentials({
-	"urn:uuid:8b82d730-44af-4305-ba01-c487ff5af1a0": {
-		username: "username",
-		password: "password",
-	},
-})
+
 
 servient.addServer(new HttpServer(httpConfig))
 const WoTHelpersServer = new Helpers(servient)
@@ -42,6 +34,9 @@ servient.start()
 .then( async (WoT) => {
 	const thing = await WoT.produce(thingDescriptionObject)
 	thing.setPropertyReadHandler("status", async () => "testing")
+	thing.setPropertyObserveHandler("status", async () => {
+		console.log("observing")
+		return "observation"})
 	await thing.expose()
 	console.info(`${thing.getThingDescription().title} ready`)
 })
